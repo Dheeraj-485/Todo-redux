edb34a8cff0c0cb894718dda842d939bdc8d9a8f
Dheeraj-485 <118504120+Dheeraj-485@users.noreply.github.com>
2024-03-30 09:26:03 +0530

    Add files via upload

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = [
+  // { id: 1, title: "Play Cricket", completed: "true" },
+  // { id: 2, title: "Learn React", completed: "true" },
+];
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload.id);
+    },
+    toggleCompleted: (state, action) => {
+      state.forEach((todo) => {
+        if (todo.id === action.payload.id) {
+          todo.completed = !todo.completed;
+        }
+      });
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleCompleted } = todosSlice.actions;
+export default todosSlice.reducer;
